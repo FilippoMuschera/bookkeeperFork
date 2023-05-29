04cce02edf5562cb6923b6c5bcdfaedf9118dd4e
filippo <filippo.99.fm@gmail.com>
2023-05-29 18:51:19 +0200

    Implemented badua, completed Test cases in existing classes

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieImplMountLedgerTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieImplMountLedgerTest.java
@@ -21,22 +21,69 @@ public class BookieImplMountLedgerTest {
 
     @Parameterized.Parameters
     public static Collection<Object[]> getParams() {
-        return Arrays.asList(new Object[][] {
+        return Arrays.asList(new Object[][]{
 
-                   //LeadgerStorage        UsageThreshold         WarnThreshold       ExpectedValue
-                {LedgerStorageType.VALID,       0.95f,                0.90f,            ExpectedValue.PASSED },
-                {LedgerStorageType.VALID,       0.20f,                0.90f,            ExpectedValue.SOME_EXCEPTION }
+                //LedgerStorage        UsageThreshold         WarnThreshold       ExpectedValue
+         {LedgerStorageType.SORTED,         -1.0f,                 -1.1f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.SORTED,         -1.0f,                 -1.0f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.SORTED,         -1.0f,                  0.0f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.SORTED,          0.0f,                 -1.0f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.SORTED,          0.0f,                  0.0f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.SORTED,          0.0f,                  1.0f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.SORTED,          0.1f,                  0.0f,        ExpectedValue.PASSED},
+         {LedgerStorageType.INTERLEAVED,     0.1f,                  0.1f,        ExpectedValue.PASSED},
+         {LedgerStorageType.INTERLEAVED,     0.1f,                  0.2f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.INTERLEAVED,     1.0f,                  0.9f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.INTERLEAVED,     1.0f,                  1.0f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.INTERLEAVED,     1.0f,                  1.1f,        ExpectedValue.ILLEGAL_ARGUMENT},
+         {LedgerStorageType.DB,              0.9f,                  0.8f,        ExpectedValue.PASSED},
+         {LedgerStorageType.NULL,            0.9f,                  0.8f,        ExpectedValue.PASSED},
+
+
+
+
+
 
         });
     }
 
-    public BookieImplMountLedgerTest( LedgerStorageType type, float usageThreshold, float warnThreshold, ExpectedValue expectedValue) {
-        //TODO TYPE LS
-        this.ledgerStorage = new SortedLedgerStorage(); //è quello di default secondo la documentazione, magari si può provare anche con gli altri valori TODO
+    public BookieImplMountLedgerTest(LedgerStorageType type, float usageThreshold, float warnThreshold, ExpectedValue expectedValue) {
+
+        this.ledgerStorage = this.getRequestedLedger(type);
         this.serverConfiguration = TestBKConfiguration.newServerConfiguration();
         serverConfiguration.setDiskUsageThreshold(usageThreshold);
         serverConfiguration.setDiskUsageWarnThreshold(warnThreshold);
         this.expectedValue = expectedValue;
+    }
+
+    private LedgerStorage getRequestedLedger(LedgerStorageType type) {
+        String ls = "";
+        LedgerStorage ledgerStorage = null;
+        switch (type) {
+            case INTERLEAVED:
+                ls = "org.apache.bookkeeper.bookie.InterleavedLedgerStorage";
+                break;
+            case SORTED:
+                ls = "org.apache.bookkeeper.bookie.SortedLedgerStorage";
+                break;
+            case DB:
+                ls = "org.apache.bookkeeper.bookie.storage.ldb.DbLedgerStorage";
+                break;
+            case NULL:
+                return null;
+
+
+        }
+        try {
+            ledgerStorage = LedgerStorageFactory.createLedgerStorage(ls);
+
+        } catch (IOException e) {
+            System.err.println("Impossibile istanziare il ledger di tipo " + type.toString() + " con la stringa " + ls);
+            e.printStackTrace();
+        }
+
+        return ledgerStorage;
+
     }
 
     @Test
@@ -46,10 +93,13 @@ public class BookieImplMountLedgerTest {
 
         try {
             BookieImpl.mountLedgerStorageOffline(serverConfiguration, ledgerStorage);
-        } catch (Exception e) {
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
-            actualValue = ExpectedValue.SOME_EXCEPTION;
+            actualValue = ExpectedValue.ILLEGAL_ARGUMENT;
 
+        } catch (IOException e) {
+            System.err.println("IOException verificatasi durante l'esecuzione di mountLedgerTest()");
+            e.printStackTrace();
         }
 
         Assert.assertEquals(expectedValue, actualValue);
@@ -57,18 +107,11 @@ public class BookieImplMountLedgerTest {
 
     }
 
-    /*
-     * Si può provare con il leadger storage null
-     *
-     * Si possono provare diverse combinazioni per diskusagetreshold e diskusagewarn nel ServerConfiguration
-     */
+    private enum LedgerStorageType {
+        INTERLEAVED, SORTED, DB, NULL
+    }
 
-}
-
-enum LedgerStorageType {
-    VALID, INVALID, NULL
-}
-
-enum ExpectedValue {
-    PASSED, SOME_EXCEPTION
+    private enum ExpectedValue {
+        PASSED, ILLEGAL_ARGUMENT
+    }
 }
---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/FormatTest.java
@@ -0,0 +1,22 @@
+package org.apache.bookkeeper.bookie;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class FormatTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object> getParams() {
+        //TODO
+        return null;
+    }
+
+
+
+
+}
