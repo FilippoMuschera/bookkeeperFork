565733760e30b1f53e867e4e4ed4022884480de6
filippo <filippo.99.fm@gmail.com>
2023-06-08 13:40:48 +0200

    Found work-around for ba-dua-cli not working

--- bookkeeper-server/badua-lib/createXml.sh
+++ bookkeeper-server/badua-lib/createXml.sh
@@ -1,5 +1,5 @@
 classes_dir=$1
 
-echo java -jar ba-dua-cli-0.6.0-all.jar report -classes "$classes_dir" --sc -input "$classes_dir/coverage.ser" -xml "$classes_dir/target/baduaReport.xml"
+java -jar "$classes_dir/badua-lib/ba-dua-cli-0.6.0-all.jar" report -classes "$classes_dir" --sc -input "$classes_dir/coverage.ser" -xml "$classes_dir/target/baduaReport.xml"
 
-java -jar "ba-dua-cli-0.6.0-all.jar report" -classes "$classes_dir" --sc -input "$classes_dir/coverage.ser" -xml "$classes_dir/target/baduaReport.xml"
+mv coverage.ser ./badua-lib/
